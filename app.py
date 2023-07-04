fffd894a12971a5bf71b0e5faccdcd7ace2ec116
Samuel Gieseker <s.w.gieseker@gmail.com>
2023-07-04 14:24:54 -0400

    deploy with explicit cors

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/analyze": {"origins": "http://localhost:3000"}})
 @app.route('/')
 def home():
     return 'Welcome to PRCA!'
